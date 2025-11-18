0bbef1bd372287688105886295fa5c6474aa51bb
Sinty026 <115690057+Sinty026@users.noreply.github.com>
2025-11-18 22:43:57 +1030

    Adjusted some of the visual to make it look better
    also had to remove the multiple song choice as i couldnt get it to work

--- src/Components/DJControls.jsx
+++ src/Components/DJControls.jsx
@@ -1,15 +1,8 @@
+import "./DJcontrols.css";
+
 function DJControls({ instruments, onInstrumentsoggle, speed, onSpeedChange }) {
     return (
         <>
-            <div>
-                <select className="form-select" aria-label="Default select example">
-                <option value="1">Select a song</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-                </select>
-            </div>
-          
-
             <div className="form-check">
                 <input className="form-check-input" type="checkbox" value="" id="bassline" checked={instruments.bassline} onChange={() => onInstrumentsoggle("bassline")}/>
                 <label className="form-check-label" htmlFor="bassline">
@@ -31,9 +24,11 @@ function DJControls({ instruments, onInstrumentsoggle, speed, onSpeedChange }) {
                 </label>
             </div>
 
-            <div class="mb-2" style={{ display:"flex", alignItems:"center", gap:"8px", marginLeft:"10px" }}>
-                <label htmlFor="speed" className="form-label">Speed</label>
-                <input type="number" className="form-control" id="speed" placeholder="Speed" min="0.1" max="4" step="0.1" value={speed} onChange={(e) => onSpeedChange(e.target.value)}/>
+            <div className="speed-control">
+                <div class="mb-2" style={{ display:"flex", alignItems:"center", gap:"8px", paddingTop:"15px"}}>
+                    <label htmlFor="speed" className="form-label">Speed: </label>
+                    <input type="number" className="form-control" id="speed" placeholder="Speed" min="0.1" max="4" step="0.1" value={speed} onChange={(e) => onSpeedChange(e.target.value)}/>
+                </div>
             </div>  
         </>
     );
--- src/Components/Volume.jsx
+++ src/Components/Volume.jsx
@@ -1,11 +1,11 @@
 export default function Volume({ volume, onVolumeChange, isMuted, onMuteToggle }) {
     return (
         <>
-            <div>
+            <div >
                 <label htmlFor="volumeRange" className="form-label">Volume:</label>
                 <input type="range" className="form-range" min="0" max="1" step="0.1" onChange={onVolumeChange} disabled={isMuted} id="volumeRange" />
             </div>
-            <div className="form-check form-switch">
+            <div className="form-check form-switch" style={{ paddingTop: "30px" }}>
                 <label htmlFor="muteButton" className="form-label">Mute</label>
                 <input className="form-check-input" type="checkbox" role="switch" id="muteButton" checked={isMuted} onChange={onMuteToggle}/>
             </div>
